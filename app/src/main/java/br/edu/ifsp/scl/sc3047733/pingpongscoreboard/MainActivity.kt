bcd8320dee3861048330409171b7204dec639832
leticia <leticia.yabuuti@aluno.ifsp.edu.br>
2026-08-30 07:58:11 -0300

    Etapa 1: contador com remember

--- app/src/main/java/br/edu/ifsp/scl/sc3047733/pingpongscoreboard/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3047733/pingpongscoreboard/MainActivity.kt
@@ -3,12 +3,17 @@ package br.edu.ifsp.scl.sc3047733.pingpongscoreboard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import br.edu.ifsp.scl.sc3047733.pingpongscoreboard.ui.theme.PingPongScoreBoardTheme
@@ -16,32 +21,45 @@ import br.edu.ifsp.scl.sc3047733.pingpongscoreboard.ui.theme.PingPongScoreBoardT
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            PingPongScoreBoardTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            PlacarScreen()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun PlacarScreen() {
+    var placarA by remember { mutableStateOf(0) }
+    var placarB by remember { mutableStateOf(0) }
+
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("Jogador A: $placarA")
+        Button(onClick = {
+            placarA++
+        }) { Text("+1") }
+
+        Text("Jogador B: $placarB")
+
+        Button(onClick = {
+            placarB++
+        }) { Text("+1") }
+
+        Button(onClick = {
+            placarA = 0;
+            placarB = 0
+        }) { Text("Reiniciar partida") }
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ScoreBoardPreview() {
     PingPongScoreBoardTheme {
-        Greeting("Android")
+        PlacarScreen()
     }
 }
